3796c4d4bc0805dd4038ec06a03a1e03ad75a8bf
Troy <troy.davis@infinitaz.com>
2024-12-20 11:33:19 -0600

    Improved Context Statement and logging modified

--- v0.py
+++ v0.py
@@ -32,8 +32,8 @@ def real_time_transcription():
 
             # Ignore silent chunks
             if is_silent(audio_chunk):
-                print("... (silence detected)")
-                #continue ### CHECK THIS LATER WITH MICROPHONE TO SEE IF IT TRANSCRIBES BLANKS OR NOTHING AT ALL
+                print("Silence Detected, will not be passed to AI")
+                continue
 
             # Transcribe audio
             segments, _ = model.transcribe(audio_chunk, language="en")
@@ -46,8 +46,9 @@ def real_time_transcription():
 
                 for word in words:
                     print(word) # Each word print out
+                    transcription_buffer.append(" ") # Adds spaces after each word or no word, to ensure silence is valued
 
-                if time.time()-last_flush_time >= 3:
+                if time.time()-last_flush_time >= 3.0:
                     threeText = " ".join(transcription_buffer)
                     print(threeText)
 
@@ -60,14 +61,19 @@ def real_time_transcription():
                         model='llama3.2',
                         messages=[{
                             'role': 'user',
-                            'content': """The following is a live transcription of a call. Please indicate whether """ +
-                            """or not there is someone on the phone. It would be evident that someone is now on the """ +
-                            """phone if they are speaking and/or potentially saying things like hello?, hi, is there """ +
-                            """anyone there? If there is silence (or nothing passed in as the transcript past the colon), """ +
-                            """it is can be concluded that no one is on the phone so you should respond with NO. Please """ +
-                            """respond with only either YES if someone is on the phone or NO if no one is on the phone """ +
-                            """given the following transcription over 3 seconds (only one side of the call meaning """ +
-                            """there will not be two people on the phone):""" +threeText
+                            'content': """Your job is to take in a transcription of one end of a phone """ +
+                                       """call and detect whether or not someone is on that end of the phone. """ +
+                                       """The transcription can have either silence (represented as spaces), """ +
+                                       """words, or a mixture of both. Remember that it can be concluded that """ +
+                                       """someone is on the phone if they are talking and potentially saying """ +
+                                       """things like "Hello?", "Is there anyone there?", and so on. Also keep """ +
+                                       """in mind they do not have to be saying only introductory phrases as """ +
+                                       """stated before. If there is someone speaking, it can be assumed that """ +
+                                       """someone is on the phone due to the transcription quality. After """ +
+                                       """analyzing the transcription, please only say YES if there is someone """ +
+                                       """on the phone and NO if there is no one on the phone. In other words, """ +
+                                       """your response should only be one word long in length, as it is either """ +
+                                       """a YES or NO. Here is the following transcript:  """ + threeText
                         }]
                     )
 
